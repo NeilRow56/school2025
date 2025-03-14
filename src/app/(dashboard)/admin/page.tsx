7699e80d63ff7655444f6f0c8f0f1c1e7112eb46
NeilRow56 <neilrowland56@gmail.com>
2025-03-14 13:57:58 +0000

    Completed admin page - with all components

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,10 +1,14 @@
+import Announcements from '@/components/dashboard/announcements'
+import AttendanceChart from '@/components/dashboard/attendance-chart'
+import CountChart from '@/components/dashboard/count-chart'
+import EventCalendar from '@/components/dashboard/event-calendar'
 import UserCard from '@/components/shared/user-card'
 
 const AdminPage = () => {
   return (
     <div className='flex flex-col gap-4 p-4 md:flex-row'>
       {/* Left */}
-      <div className='flex w-full flex-col gap-8 lg:w-2/3'>
+      <div className='lg:w-2/3px-4 flex w-full flex-col gap-8'>
         {/* USER CARDS */}
         <div className='flex flex-wrap justify-between gap-4'>
           <UserCard type='student' />
@@ -12,9 +16,23 @@ const AdminPage = () => {
           <UserCard type='parent' />
           <UserCard type='administrator' />
         </div>
+        {/* MIDDLE CHARTS */}
+        <div className='flex flex-col gap-4 lg:flex-row'>
+          {/* COUNT CHART */}
+          <div className='h-[450px] w-full lg:w-1/3'>
+            <CountChart />
+          </div>
+          {/* ATTENDANCE CHART */}
+          <div className='h-[450px] w-full lg:w-2/3'>
+            <AttendanceChart />
+          </div>
+        </div>
       </div>
       {/* Right */}
-      <div className='w-full lg:w-1/3'>R</div>
+      <div className='flex w-full flex-col gap-8 lg:w-1/3'>
+        <EventCalendar />
+        <Announcements />
+      </div>
     </div>
   )
 }
